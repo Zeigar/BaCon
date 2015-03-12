14e583f2e1a03b91cf524edd69d543b01f9330a2
mhinne <mhinne@gmail.com>
2015-03-12 13:44:58 +0100

    Reversed order of demo

--- demo.m
+++ demo.m
@@ -3,10 +3,35 @@ addpath utility\;
 addpath ggm\;
 clear all;
 load demodata.mat;
-[n,p] = size(X);
+
+%% estimate P(G,K|X)
+% Posterior of conditional independence and partial correlations
+
+[n,p] = size(subcort);
+
+Z = zscore(subcort);
+S = Z'*Z;
+
+nsamples = 1000;
+
+G = eye(p); % initial sample
+Gsamples = zeros(p,p,nsamples); % conditional independencies
+Ksamples = zeros(p,p,nsamples); % precision matrices
+Rsamples = zeros(p,p,nsamples); % partial correlations
+
+for i=1:nsamples
+    [G,K] = ggm_gwish_cbf_direct(G,S,n);
+    Gsamples(:,:,i) = G;
+    Ksamples(:,:,i) = K;
+    Rsamples(:,:,i) = prec2parcor(K);
+end
+
+figure; imagesc(mean(Gsamples,3)); axis square; colormap hot; caxis([0 1]);
+figure; imagesc(mean(Rsamples,3)); axis square; colormap jet; caxis([-1 1]);
 
 %% estimate structural connectivity
 
+p = length(N);
 prior.a = 1;
 prior.b = 4; % expected density = a/(a+b)
 
@@ -35,8 +60,8 @@ axis square; colorbar;
 
 nsamples = 100;
 A_map = zeros(p);
-T = 10;
-Tr = .5^(10/nsamples);
+T = 10; % initial temperature
+Tr = .5^(10/nsamples); % temperature decay
 
 tic;
 for i=1:nsamples    
@@ -51,60 +76,22 @@ imagesc(A_map);
 colormap hot;
 axis square; colorbar;
 
-%% full distribution with edge-wise prior
-
-nsamples = 100;
-structuralsamples2 = zeros(p,p,nsamples);
-A2 = zeros(p);
-
-%% Here we specify a silly prior that favors intra-hemisphere connections
-M = zeros(p);
-M(1:34,1:34)   = 1; 
-M(35:68,35:68) = 1; 
-M(1:34,69:75)  = 1; 
-M(69:75,1:34)  = 1; 
-M(35:68,76:82) = 1; 
-M(76:82,35:68) = 1; 
-M(69:75,69:75) = 1; 
-M(76:82,76:82) = 1;
-M = 0.9*M + 0.1*~M;
-
-tic;
-for i=1:nsamples
-    sample = struct_conn_edge_prior(A2,N,[],M);
-    A2 = sample.A;
-    structuralsamples2(:,:,i) = A2;
-end
-toc;
-
-A2_expectation = squeeze(mean(structuralsamples2,3));
-
-figure;
-subplot 121;
-imagesc(M);
-colormap hot;
-axis square; colorbar;
-subplot 122;
-imagesc(A2_expectation);
-colormap hot;
-axis square; colorbar;
-
 
 %% estimate functional connectivity with structural constraint
 
 nsamples = 1000;
-functionalsamples = zeros(p,p,nsamples);
+Rsamples = zeros(p,p,nsamples);
 S = cov(X);
 df = 3 + n;
 
 tic;
 for i=1:nsamples
     K = gwishrnd(A_map + eye(p), S, df); % diagonal must be ones
-    functionalsamples(:,:,i) = prec2parcor(K);    
+    Rsamples(:,:,i) = prec2parcor(K);    
 end
 toc;
 
-R_expectation = squeeze(mean(functionalsamples,3));
+R_expectation = squeeze(mean(Rsamples,3));
 
 figure;
 imagesc(R_expectation.*(A_map+eye(p)));
@@ -160,27 +147,4 @@ end
 figure;
 plot(num_clusters);
 
-%% estimate P(G,K|X)
-% Posterior of conditional independence and partial correlations
 
-[n,p] = size(subcort);
-
-Z = zscore(subcort);
-S = Z'*Z;
-
-nsamples = 1000;
-
-G = eye(p); % initial sample
-Gsamples = zeros(p,p,nsamples); % conditional independencies
-Ksamples = zeros(p,p,nsamples); % precision matrices
-Rsamples = zeros(p,p,nsamples); % partial correlations
-
-for i=1:nsamples
-    [G,K] = ggm_gwish_cbf_direct(G,S,n);
-    Gsamples(:,:,i) = G;
-    Ksamples(:,:,i) = K;
-    Rsamples(:,:,i) = prec2parcor(K);
-end
-
-figure; imagesc(mean(Gsamples,3)); axis square; colormap hot; caxis([0 1]);
-figure; imagesc(mean(Rsamples,3)); axis square; colormap jet; caxis([-1 1]);
